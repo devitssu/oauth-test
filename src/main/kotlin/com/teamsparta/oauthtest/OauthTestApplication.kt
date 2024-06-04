a259a1980469b7a1eed4dedb91f4f7bde074dc21
itssu <ssujinn96@gmail.com>
2024-06-04 09:26:38 +0900

    :tada: init

---
+++ src/main/kotlin/com/teamsparta/oauthtest/OauthTestApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.oauthtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OauthTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<OauthTestApplication>(*args)
+}
